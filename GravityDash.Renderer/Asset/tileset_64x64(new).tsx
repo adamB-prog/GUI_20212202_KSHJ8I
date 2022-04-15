b9d2fc4d50d52cfbb14ddfaff903c5b4ebc0c0de
Básthy Ádám László <basthyadi@stud.uni-obuda.hu>
2022-04-15 19:15:26 +0200

    Add Testing Asset

---
+++ GravityDash.Renderer/Asset/tileset_64x64(new).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset_64x64(new)" tilewidth="64" tileheight="64" tilecount="72" columns="12">
+ <image source="tileset_64x64(new).png" width="768" height="384"/>
+</tileset>
