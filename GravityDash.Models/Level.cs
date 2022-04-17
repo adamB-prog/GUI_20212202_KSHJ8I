5fa0d19278a8e814a70787bfdec2231ae49c7860
veladam01 <veladam01@gmail.com>
2022-04-17 22:25:10 +0200

    Level.cs created in Model layer

---
+++ GravityDash.Models/Level.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GravityDash.Models
+{
+    public class Level
+    {
+        public int[,] DrawingLayer { get; set; }
+        public int[,] CollisionLayer { get; set; }
+
+        public Brush[] Brushes { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
