a8c5800811af476fa385e1520d4c2a0d1daf3c7a
Básthy Ádám László <basthyadi@stud.uni-obuda.hu>
2022-04-19 11:19:38 +0200

    Create Viewport

---
+++ GravityDash.Renderer/ViewPort.cs
@@ -0,0 +1,83 @@
+﻿using GravityDash.Models.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace GravityDash.Renderer
+{
+    public class ViewPort
+    {
+        private int x;
+        private int y;
+        private int viewportWidth;
+        private int viewportHeight;
+        private double zoom;
+        private IPosition follow;
+
+        public int X { get { return x; } private set { x = value; } }
+        public int Y { get { return y; } private set { y = value; } }
+
+        public double Zoom { get { return zoom; } private set { zoom = value; } }
+
+        //public int CenterX { get { return (int)((viewportWidth + X) / 2); } }
+        //public int CenterY { get { return (int)((viewportHeight + Y) / 2); } }
+
+        public int ViewPortWidthZoomed { get { return (int)(viewportWidth / zoom); } }
+        public int ViewPortHeightZoomed { get { return (int)(viewportHeight / zoom); } }
+
+        public ViewPort(int startX, int startY, int width, int height, IPosition follow)
+        {
+            x = startX;
+            y = startY;
+            viewportHeight = height;
+            viewportWidth = width;
+            zoom = 1;
+            this.follow = follow;
+        }
+        public void ResizeViewPort(int width, int height)
+        {
+            viewportWidth = width;
+            viewportHeight = height;
+        }
+        //public void Move()
+        //{
+        //    if (Keyboard.IsKeyDown(Key.Down))
+        //    {
+        //        Y += 5;
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Up))
+        //    {
+        //        Y -= 5;
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Left))
+        //    {
+        //        X -= 5;
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Right))
+        //    {
+        //        X += 5;
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Z))
+        //    {
+
+        //        Zoom += 0.01;
+
+
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.H))
+        //    {
+
+        //        Zoom -= 0.01;
+
+        //    }
+            
+        //}
+        public void Follow()
+        {
+            
+            x = Math.Clamp((int)(-follow.X * zoom + viewportWidth / 2), -1000, -0);
+            y = Math.Clamp((int)(-follow.Y * zoom + viewportHeight / 2), -1000, 0);
+            
+        }
+    }
+}
