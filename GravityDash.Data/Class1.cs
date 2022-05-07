27b710777a524f90e12b7b2a6c8bb2a4ea950187
Básthy Ádám László <basthyadi@stud.uni-obuda.hu>
2022-05-07 17:33:34 +0200

    Add ScoreData

--- GravityDash.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GravityDash.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ GravityDash.Data/ScoreData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GravityDash.Data
+{
+    public class ScoreData
+    {
+        public IList<TimeSpan> Scores { get; set; }
+
+
+        public ScoreData()
+        {
+            StreamReader s = new StreamReader("Scores.txt");
+            Scores = new List<TimeSpan>();
+            while (!s.EndOfStream)
+            {
+                TimeSpan time = TimeSpan.Parse(s.ReadLine());
+                Scores.Add(time);
+            }
+
+            Scores = Scores.OrderByDescending(x => x.TotalMilliseconds).ToList();
+        }
+
+        public void SaveScores()
+        {
+            IList<string> scores = new List<string>();
+
+            for (int i = 0; i < Scores.Count; i++)
+            {
+                scores.Add(Scores[i].ToString());
+            }
+            File.Delete("Scores.txt");
+            File.WriteAllLines("Scores.txt", scores);
+        }
+
+        public void AddScore(TimeSpan score)
+        {
+            Scores.Add(score);
+            Scores = Scores.OrderByDescending(x => x.TotalMilliseconds).ToList();
+        }
+
+        public string GetHighScore()
+        {
+            TimeSpan score = Scores[0];
+            return string.Format("HighScore: {0}:{1}.{2}", score.Minutes, score.Seconds, score.Milliseconds);
+        }
+
+        public string GetScoreList()
+        {
+            string scores = "";
+            for (int i = 0; i < Scores.Count; i++)
+            {
+                scores += string.Format("{0}:{1}.{2}\n", Scores[i].Minutes, Scores[i].Seconds, Scores[i].Milliseconds);
+            }
+
+            return scores;
+        }
+    }
+}
